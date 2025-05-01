4d1d8bb6f74c2b1126c98335bc7e00503dc87308
CodeMatrix1 <srivallabhmanyam27@gmail.com>
2025-05-01 11:54:03 +0530

    Update App.jsx

--- authenticationReact/front-end/src/App.jsx
+++ authenticationReact/front-end/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             <div className="nav-logo">InstiStore</div>
             <div className="nav-links">
               <p>Home</p>
-              <p>Products</p>
+              <p>Admin</p>
               <p>About</p>
               <p>Contact</p>
             </div>
